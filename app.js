b777384a632ac271f2b69af445dac8adaeffa65d
bhy304 <bhy0512@gmail.com>
2019-08-13 01:57:18 +0900

    Add save image feature

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 700;
-canvas.height = 700; 
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 700;
 
-ctx.strokeStyle = "#2c2c2c";
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE; 
+
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -39,6 +46,7 @@ function onMouseMove(event) {
 function handleColorClick(event) {
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
 }
 
 function handleRangeChange(event) {
@@ -56,11 +64,31 @@ function handleModeClick() {
     }
 }
 
+function handleCanvasClick() {
+    if(filling) {
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }
+}
+
+function handleCM(event) {
+    event.preventDefault;
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL("image/png");
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS";
+    link.click();
+}
+
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", startPainting); //마우스를 클릭했을 때 발생
-    canvas.addEventListener("mouseup", stopPainting); //마우스를 떼는 순간 발생
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting); 
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
 
 //Changing Color 
@@ -73,6 +101,12 @@ if(range) {
     range.addEventListener("input", handleRangeChange);
 }
 
+//Filling Mode
 if(mode) {
     mode.addEventListener("click", handleModeClick);
+}
+
+//Saving the Image
+if(saveBtn) {
+    saveBtn.addEventListener("click", handleSaveClick);
 }
